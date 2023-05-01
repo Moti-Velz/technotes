d83904391f5ea2829d6f29ee782deab331e6b640
Tim <roppa070@gmail.com>
2023-05-01 00:37:40 -0400

    ok

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,11 +1,8 @@
-import { useSelector } from "react-redux";
-import { selectCurrentToken } from "../features/auth/authSlice";
-import jwtDecode from "jwt-decode";
-
-import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectCurrentToken } from "../features/auth/authSlice"
+import jwtDecode from 'jwt-decode'
 
 const useAuth = () => {
-
     const token = useSelector(selectCurrentToken)
     let isManager = false
     let isAdmin = false
@@ -26,5 +23,4 @@ const useAuth = () => {
 
     return { username: '', roles: [], isManager, isAdmin, status }
 }
-
 export default useAuth
