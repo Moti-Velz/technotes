00283c62d8afbbf045bd2981c83e78b3396bc3f1
Tim <roppa070@gmail.com>
2023-05-06 14:37:27 -0400

    yup

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://technotes-api-ftuu.onrender.com', //'http://localhost:3500'
+    baseUrl: 'http://technotes-api-ftuu.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
